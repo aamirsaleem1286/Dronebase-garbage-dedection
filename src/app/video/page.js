4dff592739872454dc524d1eacc263e8e6767710
Aamir Saleem <65162736+aamirsaleem1286@users.noreply.github.com>
2024-07-22 10:39:35 +0500

    deploy

--- src/app/video/page.js
+++ src/app/video/page.js
@@ -7,7 +7,7 @@ const Videopage = () => {
         <div className="container h-screen flex flex-col justify-center items-center">
             <h1 className="heading text-3xl font-bold mb-8">Drone View</h1>
             <div className="webcam-container w-full h-full overflow-hidden">
-                <iframe src="http://192.168.122.201:8080/browserfs.html" className="webcam-video w-full h-full" allow="autoplay" frameBorder="0" onError={(e) => console.error("Error loading iframe:", e)}></iframe>
+                <iframe src="http://192.168.122.228:8080/browserfs.html" className="webcam-video w-full h-full" allow="autoplay" frameBorder="0" onError={(e) => console.error("Error loading iframe:", e)}></iframe>
             </div>
         </div>
     );
